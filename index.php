092465d8b79e994108f7a4adffad3a97c002ed5e
dhnchrs <37863926+dhnchrs@users.noreply.github.com>
2018-05-09 10:57:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,36 +19,36 @@
 		border-bottom: 5px solid blue;
 		max-height: 500px;
 		position: absolute;
-		top: -650px;
-		left: -550px;
-		width: 530px;
+		top: -640px;
+		left: -540px;
+		width: 520px;
 	}
 	.MAB {
 		height: 400px;
 		border-bottom: 5px solid red;
 		max-height: 500px;
 		position: absolute;
-		top: -650px;
-		left: -550px;
-		width: 530px;
+		top: -640px;
+		left: -540px;
+		width: 520px;
 	}
 	.MAN {
 		height: 300px;
 		border-bottom: 5px solid yellow;
 		max-height: 500px;
 		position: absolute;
-		top: -650px;
-		left: -550px;
-		width: 530px;
+		top: -640px;
+		left: -540px;
+		width: 520px;
 	}
 	.MAMIN {
 		height: 200px;
 		border-bottom: 5px solid lightgreen;
 		max-height: 500px;
 		position: absolute;
-		top: -650px;
-		left: -550px;
-		width: 530px;
+		top: -640px;
+		left: -540px;
+		width: 520px;
 	}
 
 	#rotate_{
@@ -67,41 +67,22 @@
 				<div class="MAN"></div>
 				<div class="MAMIN"></div>
 			</div>
-			<svg height="600" width="900" style="position:absolute;top:140px;left:190px;">
-				<polygon points="360,0 800,510 10,510" style="fill:white;stroke:black;stroke-width:5" />
-			</svg>
-			<!--GARIS 45 ke bawah-->
-			<svg height="510" width="1000" style="position:absolute;top:140px;left:550px;">
-				<line x1="0" y1="0" x2="550" y2="635" style="stroke:white;stroke-width:10" />
-			</svg>
-			<!--LENGKUNG-->
-			<svg width="190" height="160" style="position:absolute;top:67px;left:535px;">
-				<path d="M10 80 Q 95 10 180 80" style="fill:white;stroke:black;stroke-width:5"/>
-			</svg>
-			<!--GARIS 45 ke bawah-->
-			<svg height="510" width="1000" style="position:absolute;top:140px;left:708px;">
-				<line x1="0" y1="0" x2="550" y2="635" style="stroke:black;stroke-width:5" />
-			</svg>
-			
-			<!--GARIS KIRI BAWAH-->
-			<svg height="210" width="500" style="position:absolute;top:450px;left:20px;">
-				<line x1="0" y1="200" x2="180" y2="200" style="stroke:black;stroke-width:5" />
-			</svg>
-			<!--GARIS TENGAH-->
-			<svg height="210" width="2000" style="position:absolute;top:450px;left:206px;">
-				<line x1="0" y1="200" x2="900" y2="200" style="stroke:white;stroke-width:10" />
-			</svg>
-			<!--GARIS 45 ke bawah-->
-			<svg height="210" width="500" style="position:absolute;top:640px;left:1141px;">
-				<line x1="0" y1="0" x2="180" y2="200" style="stroke:black;stroke-width:5" />
-			</svg>
-			<!--GARIS 45 ke atas-->
-			<svg height="210" width="500" style="position:absolute;top:690px;left:1318px;">
-				<line x1="0" y1="150" x2="160" y2="0" style="stroke:black;stroke-width:5" />
-			</svg>
-			<!--GARIS KANAN BAWAH-->
-			<svg height="210" width="500" style="position:absolute;top:550px;left:1240px;">
-				<line x1="0" y1="200" x2="175" y2="200" style="stroke:black;stroke-width:5" />
+			<svg height="700" width="1500" style="position:absolute;top:100px;left:20px;">
+				<g>
+					<polygon points="520,40 1000,540 200,540" style="fill:white;stroke:white;stroke-width:5" />
+					<!--GARIS KIRI BAWAH-->
+					<line x1="0" y1="200" x2="200" y2="200" style="stroke:black;stroke-width:5" transform="translate(0, 340)" />
+					<!--GARIS 45 ke atas-->
+					<line x1="0" y1="-100" x2="-326" y2="408" style="stroke:black;stroke-width:5;" transform="translate(524, 134)" />
+					<!--LENGKUNG-->
+					<line x1="0" y1="-100" x2="634" y2="560" style="stroke:black;stroke-width:5;" transform="translate(679, 136)" />
+					<!--GARIS 45 ke bawah-->
+					<path d="M20 80 Q 95 10 180 80" style="fill:white;stroke:black;stroke-width:5;" transform="translate(501, -42)" />
+					<!--GARIS 45 ke atas-->
+					<line x1="0" y1="250" x2="150" y2="100" style="stroke:black;stroke-width:5" transform="translate(1310, 446)" />
+					<!--GARIS KANAN BAWAH-->
+					<line x1="0" y1="200" x2="172" y2="200" style="stroke:black;stroke-width:5" transform="translate(1228, 406)" />
+				</g>
 			</svg>
 	</div>
 </body>
